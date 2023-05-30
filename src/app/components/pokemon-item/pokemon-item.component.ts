0b5f4ba165df379f2048d81eedf3a6015bfaba65
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-05-30 10:09:36 +0200

    Création du pokemon item

---
+++ src/app/components/pokemon-item/pokemon-item.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'pokedex-pokemon-item',
+  templateUrl: './pokemon-item.component.html',
+})
+export class PokemonItemComponent {
+
+}
